d32ce5f7233fa43fcf22cd26fdecf4adefbe85ec
Dino <dino.devedzic2018@gmail.com>
2026-03-12 20:38:52 +0100

    UFO_Zadatak(za samostalan rad)

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 import folium
 
-# df['shape'] = df['shape'].str.lower().str.strip()
-# df['city'] = df['city'].str.lower().str.strip()
-# df['country'] = df['country'].str.lower().str.strip()
-
-df = pd.read_csv('ufo_sightings.csv')
-
+df = pd.read_csv("ufo_sightings.csv", low_memory=False)
+# koordianti u brojeve
 df["latitude"] = pd.to_numeric(df["latitude"], errors="coerce")
 df["longitude"] = pd.to_numeric(df["longitude"], errors="coerce")
-# Remove rows with invalid coordinates
+df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
+df["city"] = df["city"].fillna("Unknown")
+df["country"] = df["country"].fillna("Unknown")
+df["shape"] = df["shape"].fillna("Unknown")
+
+
+# brisanje redova sa nedostajućim koordinatama
 df = df.dropna(subset=["latitude", "longitude"])
 
 df = df.sample(5000)
-
+# mapa
 ufo_map = folium.Map(location=[20, 0], zoom_start=2)
+
+# dodavanje markera
 for index, row in df.iterrows():
+    year = row["datetime"].year if pd.notnull(row["datetime"]) else "Unknown"
+    popup_text = (
+        f"<b>City:</b> {row['city']}<br>"
+        f"<b>Country:</b> {row['country']}<br>"
+        f"<b>Shape:</b> {row['shape']}<br>"
+        f"<b>Year:</b> {year}"
+    )
     folium.CircleMarker(
         location=[row["latitude"], row["longitude"]],
         radius=2,
-        popup=row["city"],
+        popup=folium.Popup(popup_text, max_width=200),
+        tooltip=popup_text,
         color="red"
     ).add_to(ufo_map)
 ufo_map.save("ufo_map.html")
